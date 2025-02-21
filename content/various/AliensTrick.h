1ab9d108ee1c6bff2ef1fdf738f2431a4e9713fe
Adam Sołtan <adam.henryk.soltan@gmail.com>
2025-02-21 18:18:16 +0100

    aliens trick

---
+++ content/various/AliensTrick.h
@@ -0,0 +1,20 @@
+/**
+ * Author: Antek Dlugosz
+ * License: CC0
+ * Source: folklore
+ * Description: Optimize dp where you want "k things with minimal cost". The slope of f(k) must be
+ non increasing. Provide a function g(lambda) that computes the best answer for any k with costs increased by lambda.
+ * Status: tested on https://codeforces.com/contest/1197/problem/C and https://codeforces.com/contest/1279/problem/F
+ */
+#pragma once
+
+ll aliens(ll k, auto g) { // returns f(k)
+	// make sure [l, r) is ok (r > max slope etc)
+	ll l = 0, r = 1e11;
+	while (l + 1 < r) {
+		ll m = (l + r) / 2;
+		(g(m - 1) + k <= g(m) ? l : r) = m;
+	}
+	// return l if you want the optimal lambda
+	return g(l) - l * k;
+}
