e4944373eb3b01163cef8cdc250f95b368c0b24b
Adam Soltan <adam.henryk.soltan@gmail.com>
2025-04-07 14:15:25 +0200

    fix warning

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -15,7 +15,7 @@ struct Tree {
 	static constexpr T unit = INT_MIN;
 	T f(T a, T b) { return max(a, b); } // (any associative fn)
 	vector<T> s; int n;
-	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
+	Tree(int N = 0, T def = unit) : s(2*N, def), n(n) {}
 	void update(int pos, T val) {
 		for (s[pos += n] = val; pos /= 2;)
 			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
